f7da1921af04c82fe2b1465a5335c5d5f91bcf96
lol-cubes <arinmkhare@gmil.com>
2019-11-12 20:59:51 -0500

    increase version number

--- cl-timer.rb
+++ cl-timer.rb
@@ -3,7 +3,7 @@ class ClTimer < Formula
     homepage "https://lol-cubes/github.io/cl-timer"
     url "https://github.com/lol-cubes/cl-timer/raw/master/cl-timer-1.0.5.tar.gz"
     sha256 "75edf16b019a6aca66f2bd83ee757203e45fca1bd5faa702a961950ea04ba37c"
-    version "1.0.5"
+    version "1.0.6"
 
     bottle :unneeded
   
