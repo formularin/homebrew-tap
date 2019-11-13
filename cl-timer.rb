77f49ea42d4216bb1ab932acbcd0b1c5ba3a70ac
lol-cubes <arinmkhare@gmil.com>
2019-11-12 20:22:06 -0500

    initial commit

---
+++ cl-timer.rb
@@ -0,0 +1,13 @@
+class CLTimer < Formula
+    desc "A cubing timer for the terminal"
+    homepage "https://lol-cubes/github.io/cl-timer"
+    url "https://github.com/lol-cubes/cl-timer/raw/master/cl-timer-1.0.5.tar.gz"
+    sha256 "75edf16b019a6aca66f2bd83ee757203e45fca1bd5faa702a961950ea04ba37c"
+    version "1.0.5"
+
+    bottle :unneeded
+  
+    def install
+      bin.install "cl-timer"
+    end
+  end
