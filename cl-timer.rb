326a5d0a1611f9c88e591307cdcc359752547484
lol-cubes <arinmkhare@gmil.com>
2019-11-12 22:30:48 -0500

    update sha256

--- cl-timer.rb
+++ cl-timer.rb
@@ -2,7 +2,7 @@ class ClTimer < Formula
     desc "A cubing timer for the terminal"
     homepage "https://lol-cubes/github.io/cl-timer"
     url "https://github.com/lol-cubes/cl-timer/raw/master/cl-timer-1.0.6.tar.gz"
-    sha256 "75edf16b019a6aca66f2bd83ee757203e45fca1bd5faa702a961950ea04ba37c"
+    sha256 "da7061b8855a28859b7d6b6efdcf93c6f0212b9f27e1db8f5c83725684e871b1"
     version "1.0.6"
 
     bottle :unneeded
