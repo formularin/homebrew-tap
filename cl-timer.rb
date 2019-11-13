5a34d86bfba3e4169045b06a86491ecbc24ddff1
lol-cubes <arinmkhare@gmil.com>
2019-11-12 22:28:57 -0500

    change archive file location

--- cl-timer.rb
+++ cl-timer.rb
@@ -1,7 +1,7 @@
 class ClTimer < Formula
     desc "A cubing timer for the terminal"
     homepage "https://lol-cubes/github.io/cl-timer"
-    url "https://github.com/lol-cubes/cl-timer/raw/master/cl-timer-1.0.5.tar.gz"
+    url "https://github.com/lol-cubes/cl-timer/raw/master/cl-timer-1.0.6.tar.gz"
     sha256 "75edf16b019a6aca66f2bd83ee757203e45fca1bd5faa702a961950ea04ba37c"
     version "1.0.6"
 
