c89f6c0693b2a0e6b6bbd5e5ca085c54f411e4f8
lol-cubes <arinmkhare@gmil.com>
2019-11-14 17:54:31 -0500

    change archive url to release in main repo

--- cl-timer.rb
+++ cl-timer.rb
@@ -1,7 +1,7 @@
 class ClTimer < Formula
     desc "A cubing timer for the terminal"
     homepage "https://lol-cubes/github.io/cl-timer"
-    url "https://github.com/lol-cubes/cl-timer/raw/master/cl-timer-1.0.7.tar.gz"
+    url "https://github.com/lol-cubes/cl-timer/files/3849017/cl-timer-1.0.7.tar.gz"
     sha256 "aa86a66460b3e9351db28d8ef798b017814f48a574ed44bf75db50ddc47a3786"
     version "1.0.7"
 
