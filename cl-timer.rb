ff998e181cb010d3b33e50d5369ee2746c5321d2
lol-cubes <arinmkhare@gmil.com>
2019-11-27 16:11:25 -0500

    update version to fix small bug

--- cl-timer.rb
+++ cl-timer.rb
@@ -1,9 +1,9 @@
 class ClTimer < Formula
     desc "A cubing timer for the terminal"
     homepage "https://lol-cubes/github.io/cl-timer"
-    url "https://github.com/lol-cubes/cl-timer/releases/download/v1.1.3/cl-timer-1.1.3.tar.gz"
-    sha256 "ff01394e8895014f67a893d96142ce9ad37665bb36dacc94d758047a8344a0f6"
-    version "1.1.3"
+    url "https://github.com/lol-cubes/cl-timer/releases/download/v1.1.4/cl-timer-1.1.4.tar.gz"
+    sha256 "b917d9a60d0250a451a859356f00fbdc11f4fa77c69617ad19fddcc691345122"
+    version "1.1.4"
 
     bottle :unneeded
   
